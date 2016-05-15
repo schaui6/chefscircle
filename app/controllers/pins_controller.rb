28b43660cfa7bf07f112d33b20583190fa20a4a9
schaui6 <sanderfer.chau@gmail.com>
2016-05-15 16:26:32 -0700

    Pin CRUD features

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,8 @@
 class PinsController < ApplicationController
+  before_action :find_pin, only: [:show, :edit, :update, :destroy]
 
   def index
+    @pins = Pin.all.order('created_at DESC')
   end
 
   def new
@@ -9,12 +11,41 @@ class PinsController < ApplicationController
 
   def create
     @pin = Pin.new(pin_params)
+
+    if @pin.save
+      redirect_to @pin, notice: 'Successfully saved new pin!'
+    else
+      render 'new'
+    end
   end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: 'Pin was successfully updated'
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @pin.destroy
+    redirect_to root_path
+  end 
 
   private
 
   def pin_params
-    params.require(:pin).permit(:title, :descriptionå)
+    params.require(:pin).permit(:title, :description)
+  end
+
+  def find_pin
+    @pin = Pin.find(params[:id])
   end
 
 end
